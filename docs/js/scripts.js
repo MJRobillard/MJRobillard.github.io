6c30c572a69272d13020d1c500b62c9d9dcd01db
Matthew Robillard <robillard.matthew22@gmail.com>
2023-08-01 08:37:52 -0400

    loaded

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -71,8 +71,11 @@ window.onload = () => {
       setTimeout(() => {
         preloader.style.display = 'none';
         pageContainer.style.display = 'block';
-      }, 2000);
-    }
+      }, 1000);
+      preloader.classList.add('loaded');
+
+    };
+
   
     // Rest of the existing JavaScript code...
   
@@ -156,58 +159,7 @@ window.onload = () => {
   
     /**
      * Init swiper slider with 1 slide at once in desktop view
-     */
-    new Swiper('.slides-1', {
-      speed: 600,
-      loop: true,
-      autoplay: {
-        delay: 5000,
-        disableOnInteraction: false
-      },
-      slidesPerView: 'auto',
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      }
-    });
-  
-    /**
-     * Init swiper slider with 3 slides at once in desktop view
-     */
-    new Swiper('.slides-3', {
-      speed: 600,
-      loop: true,
-      autoplay: {
-        delay: 5000,
-        disableOnInteraction: false
-      },
-      slidesPerView: 'auto',
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 40
-        },
-  
-        1200: {
-          slidesPerView: 3,
-        }
-      }
-    });
-  
+
     /**
      * Animation on scroll function and init
      */
